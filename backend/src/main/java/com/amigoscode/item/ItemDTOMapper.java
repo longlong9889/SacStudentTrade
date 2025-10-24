3f1e16a4c176336e142cd21f12a9291d619aeaad
longn <longnguyen7803@gmail.com>
2025-10-23 19:26:52 -0700

    Added mapper to convert Item entity -> ItemDTO

---
+++ backend/src/main/java/com/amigoscode/item/ItemDTOMapper.java
@@ -0,0 +1,26 @@
+package com.amigoscode.item;
+
+import org.springframework.stereotype.Service;
+import java.util.function.Function;
+
+@Service
+public class ItemDTOMapper implements Function<Item, ItemDTO> {
+
+    @Override
+    public ItemDTO apply(Item item) {
+        return new ItemDTO(
+                item.getId(),
+                item.getTitle(),
+                item.getDescription(),
+                item.getPrice(),
+                item.getCategory(),
+                item.getImageId(),
+                item.getStatus(),
+                item.getSeller().getId(),
+                item.getSeller().getName(),
+                item.getSeller().getEmail(),
+                item.getCreatedAt(),
+                item.getUpdatedAt()
+        );
+    }
+}
